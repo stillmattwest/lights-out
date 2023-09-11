7b68ea4bab2b5e120ae5d7c1deeb7295a87f7963
Matt West <stillmattwest@gmail.com>
2023-09-10 20:35:39 -0700

    adding puzzle object

---
+++ src/puzzles/puzzles.js
@@ -0,0 +1,12 @@
+
+
+const puzzles = {
+        centerThree:{
+            grid:[[2,0],[2,2],[2,4]],
+            difficulty:'easy'
+        },
+        singleCorner:{
+            grid:[[0,0]],
+            difficulty:'easy'
+        }
+}
