b34f2c14eec8d3333fdf0ded9904ce89ec41f025
stillmattwest <stillmattwest@gmail.com>
2023-09-14 22:07:28 -0700

    adding grid disabled until game start

--- src/components/LightSquare/LightSquare.jsx
+++ src/components/LightSquare/LightSquare.jsx
@@ -1,8 +1,18 @@
 import './LightSquare.css';
 
+import { useGameContext } from '../../context/GameContext';
+
 const LightSquare = (props) => {
+
+    const { start } = useGameContext();
+
+    const clickHander = (e) => {
+        if (start) {
+            props.toggleSquareHandler(e);
+        }
+    }
     return (
-        <div className={`light-square ${props.isLit ? 'lit' : ''}`} row={props.row} col={props.col} onClick={props.toggleSquareHandler}>
+        <div className={`light-square ${props.isLit ? 'lit' : ''}`} row={props.row} col={props.col} onClick={(e) => clickHander(e)}>
 
         </div>
     )
